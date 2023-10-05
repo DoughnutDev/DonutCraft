9162483b873898b9e40208b0318c7748b92884ef
Gunnar Stinson <gunnarmstinson@gmail.com>
2023-10-05 11:22:48 -0400

    Update for DonutCraft-1.20-0.0.1

---
+++ kubejs/server_scripts/example.js
@@ -0,0 +1,5 @@
+// priority: 0
+
+// Visit the wiki for more info - https://kubejs.com/
+
+console.info('Hello, World! (Loaded server scripts)')
---
+++ kubejs/server_scripts/testing.js
@@ -0,0 +1,184 @@
+ServerEvents.recipes(event => {
+
+
+//Replacing Create Andesite with GregTech Andesite
+    event.replaceInput(
+        { input: 'create:andesite_alloy' }, // Arg 1: the filter
+        'create:andesite_alloy',            // Arg 2: the item to replace
+        'gtceu:andesite_alloy_ingot'         // Arg 3: the item to replace it with
+        // Note: tagged fluid ingredients do not work on Fabric, but tagged items do.
+      )
+
+//Removes Andesite
+    event.remove({ input: 'create:andesite_alloy' })
+    
+
+
+//Adding Recipe for Andesite Alloy Dust
+    event.shaped(
+        Item.of('gtceu:andesite_alloy_dust', 4),
+        [
+            'BA ',
+            'AB ',
+            '  C'
+        ],
+        {
+            A: 'minecraft:iron_nugget',
+            B: 'minecraft:andesite',
+            C: '#forge:tools/mortars'
+        }
+    )
+    
+//Add Mixer Recipe for Andesite Alloy Dust
+    event.recipes.gtceu.mixer('andesite_alloy_dust_mixer')
+    .itemInputs('2x minecraft:iron_nugget', '2x minecraft:andesite')
+    .itemOutputs('6x gtceu:andesite_alloy_dust')
+    .duration(24)
+    .EUt(16)
+
+////// Machine Recipe //////
+
+event.shaped(
+    'gtceu:greenhouse',
+    ['AWA', 'CSC', 'WCW'],
+    {
+        A: '#forge:circuits/mv',
+        W: 'gtceu:copper_single_cable',
+        C: '#forge:circuits/mv',
+        S: 'gtceu:solid_machine_casing'
+    }
+).id('gtceu:shaped/greenhouse')
+
+
+////// Greenhouse Recipes //////
+
+function Greenhouse(id, input, fluid, output, boosted) {
+    if (boosted) {
+        event.recipes.gtceu.greenhouse(id)
+            .circuit(2)
+            .notConsumable(InputItem.of(input))
+            .itemInputs('4x gtceu:fertilizer')
+            .inputFluids(Fluid.of('minecraft:water', fluid))
+            .itemOutputs(output)
+            .duration(320)
+            .EUt(32)
+    } else {
+        event.recipes.gtceu.greenhouse(id)
+            .circuit(1)
+            .notConsumable(InputItem.of(input))
+            .inputFluids(Fluid.of('minecraft:water', fluid))
+            .itemOutputs(output)
+            .duration(640)
+            .EUt(32)
+    }
+}
+
+
+////// Trees //////
+
+// Rubber
+Greenhouse('rubber_sapling', 'gtceu:rubber_sapling', 1000, ['32x gtceu:rubber_log', '8x gtceu:sticky_resin', '4x gtceu:rubber_sapling'], false)
+Greenhouse('rubber_sapling_boosted', 'gtceu:rubber_sapling', 1000, ['64x gtceu:rubber_log', '16x gtceu:sticky_resin', '4x gtceu:rubber_sapling'], true)
+
+// Oak
+Greenhouse('oak_sapling', 'minecraft:oak_sapling', 1000, ['64x minecraft:oak_log', '4x minecraft:oak_sapling'], false)
+Greenhouse('oak_sapling_boosted', 'minecraft:oak_sapling', 1000, ['64x minecraft:oak_log', '64x minecraft:oak_log', '4x minecraft:oak_sapling'], true)
+
+// Dark Oak
+Greenhouse('dark_oak_sapling', 'minecraft:dark_oak_sapling', 1000, ['64x minecraft:dark_oak_log', '4x minecraft:dark_oak_sapling'], false)
+Greenhouse('dark_oak_sapling_boosted', 'minecraft:dark_oak_sapling', 1000, ['64x minecraft:dark_oak_log', '64x minecraft:dark_oak_log', '4x minecraft:dark_oak_sapling'], true)
+
+// Spruce
+Greenhouse('spruce_sapling', 'minecraft:spruce_sapling', 1000, ['64x minecraft:spruce_log', '4x minecraft:spruce_sapling'], false)
+Greenhouse('spruce_sapling_boosted', 'minecraft:spruce_sapling', 1000, ['64x minecraft:spruce_log', '64x minecraft:spruce_log', '4x minecraft:spruce_sapling'], true)
+
+// Birch
+Greenhouse('birch_sapling', 'minecraft:birch_sapling', 1000, ['64x minecraft:birch_log', '4x minecraft:birch_sapling'], false)
+Greenhouse('birch_sapling_boosted', 'minecraft:birch_sapling', 1000, ['64x minecraft:birch_log', '64x minecraft:birch_log', '4x minecraft:birch_sapling'], true)
+
+// Acacia
+Greenhouse('acacia_sapling', 'minecraft:acacia_sapling', 1000, ['64x minecraft:acacia_log', '4x minecraft:acacia_sapling'], false)
+Greenhouse('acacia_sapling_boosted', 'minecraft:acacia_sapling', 1000, ['64x minecraft:acacia_log', '64x minecraft:acacia_log', '4x minecraft:acacia_sapling'], true)
+
+// Jungle
+Greenhouse('jungle_sapling', 'minecraft:jungle_sapling', 1000, ['64x minecraft:jungle_log', '4x minecraft:jungle_sapling'], false)
+Greenhouse('jungle_sapling_boosted', 'minecraft:jungle_sapling', 1000, ['64x minecraft:jungle_log', '64x minecraft:jungle_log', '4x minecraft:jungle_sapling'], true)
+
+// Azalea
+Greenhouse('azalea_sapling', 'minecraft:azalea', 1000, ['64x minecraft:oak_log', '4x minecraft:azalea'], false)
+Greenhouse('azalea_boosted', 'minecraft:azalea', 1000, ['64x minecraft:oak_log', '64x minecraft:oak_log', '4x minecraft:azalea'], true)
+
+// Flowering Azalea
+Greenhouse('flowering_azalea', 'minecraft:flowering_azalea', 1000, ['64x minecraft:oak_log', '4x minecraft:flowering_azalea'], false)
+Greenhouse('flowering_azalea_boosted', 'minecraft:flowering_azalea', 1000, ['64x minecraft:oak_log', '64x minecraft:oak_log', '4x minecraft:flowering_azalea'], true)
+
+// Mangrove
+Greenhouse('mangrove_propagule', 'minecraft:mangrove_propagule', 1000, ['64x minecraft:mangrove_log', '4x minecraft:mangrove_propagule'], false)
+Greenhouse('mangrove_propagule_boosted', 'minecraft:mangrove_propagule', 1000, ['64x minecraft:mangrove_log', '64x minecraft:mangrove_log', '4x minecraft:mangrove_propagule'], true)
+
+////// Crops //////
+
+// Sugarcane
+Greenhouse('sugar_cane', 'minecraft:sugar_cane', 1000, '24x minecraft:sugar_cane', false)
+Greenhouse('sugar_cane_boosted', 'minecraft:sugar_cane', 1000, '48x minecraft:sugar_cane', true)
+
+// Kelp
+Greenhouse('kelp', 'minecraft:kelp', 2000, '24x minecraft:kelp', false)
+Greenhouse('kelp_boosted', 'minecraft:kelp', 2000, '48x minecraft:kelp', true)
+
+// Bamboo
+Greenhouse('bamboo', 'minecraft:bamboo', 1000, '24x minecraft:bamboo', false)
+Greenhouse('bamboo_boosted', 'minecraft:bamboo', 1000, '48x minecraft:bamboo', true)
+
+// Cactus
+Greenhouse('cactus', 'minecraft:cactus', 1000, '24x minecraft:cactus', false)
+Greenhouse('cactus_boosted', 'minecraft:cactus', 1000, '48x minecraft:cactus', true)
+
+// Wheat
+Greenhouse('wheat', 'minecraft:wheat_seeds', 1000, '24x minecraft:wheat', false)
+Greenhouse('wheat_boosted', 'minecraft:wheat_seeds', 1000, '48x minecraft:wheat', true)
+
+// Carrot
+Greenhouse('carrot', 'minecraft:carrot', 1000, '24x minecraft:carrot', false)
+Greenhouse('carrot_boosted', 'minecraft:carrot', 1000, '48x minecraft:carrot', true)
+
+// Potato
+Greenhouse('potato', 'minecraft:potato', 1000, '24x minecraft:potato', false)
+Greenhouse('potato_boosted', 'minecraft:potato', 1000, '48x minecraft:potato', true)
+
+// Beetroot
+Greenhouse('beetroot', 'minecraft:beetroot_seeds', 1000, '24x minecraft:beetroot', false)
+Greenhouse('beetroot_boosted', 'minecraft:beetroot_seeds', 1000, '48x minecraft:beetroot', true)
+
+// Mellon
+Greenhouse('melon', 'minecraft:melon_seeds', 1000, '12x minecraft:melon', false)
+Greenhouse('melon_boosted', 'minecraft:melon_seeds', 1000, '24x minecraft:melon', true)
+
+// Pumpkin
+Greenhouse('pumpkin', 'minecraft:pumpkin_seeds', 1000, '12x minecraft:pumpkin', false)
+Greenhouse('pumpkin_boosted', 'minecraft:pumpkin_seeds', 1000, '24x minecraft:pumpkin', true)
+
+// Nether Wart
+Greenhouse('nether_wart', 'minecraft:nether_wart', 1000, '12x minecraft:nether_wart', false)
+Greenhouse('nether_wart_boosted', 'minecraft:nether_wart', 1000, '24x minecraft:nether_wart', true)
+
+// Red Mushroom
+Greenhouse('red_mushroom', 'minecraft:red_mushroom', 1000, '12x minecraft:red_mushroom', false)
+Greenhouse('red_mushroom_boosted', 'minecraft:red_mushroom', 1000, '24x minecraft:red_mushroom', true)
+
+// Brown Mushroom
+Greenhouse('brown_mushroom', 'minecraft:brown_mushroom', 1000, '12x minecraft:brown_mushroom', false)
+Greenhouse('brown_mushroom_boosted', 'minecraft:brown_mushroom', 1000, '24x minecraft:brown_mushroom', true)
+
+
+
+
+
+
+
+
+
+
+
+    console.log('Hello! The recipe events are ready to rumble!')
+})
